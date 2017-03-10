a0a2dd1d5bf57a76b3f9b1a5f63e6bcfc040b12f
Jordan Richlen <Jmrichlen@gmail.com>
2017-03-09 20:52:49 -0600

    Added acceptCoin tests

--- VendingMachine/test/test.js
+++ VendingMachine/test/test.js
@@ -10,4 +10,10 @@ describe('VendingMachine', function () {
         var vendor = new VendingMachine();
         assert.equal('insert coin', vendor.display);
     });
-})
+    describe('#acceptCoin', function () {
+        it('should return "$0.25"');
+        it('should return "$0.10"');
+        it('should return "$0.05"');
+        it('should return the Penny');
+    });
+});
