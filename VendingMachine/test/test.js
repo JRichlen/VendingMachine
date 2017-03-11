733c6d055cc6b27d8159064939d261e1ea0cc4a9
Jordan Richlen <Jmrichlen@gmail.com>
2017-03-11 14:40:32 -0600

    Added test to recognize that a quarter is not a nickel.

--- VendingMachine/test/test.js
+++ VendingMachine/test/test.js
@@ -15,6 +15,12 @@ describe('VendingMachine', function () {
             var vendor = new VendingMachine();
             var coin = new Quarter();
             assert(vendor.isCoin(coin, coin.weight, coin.diameter))
+        });
+        it('should recognize that a quarter is not a nickel', function () {
+            var vendor = new VendingMachine();
+            var quarter = new Quarter();
+            var nickel = new Nickel();
+            assert.equal(vendor.isCoin(quarter, nickel.weight, nickel.diameter), false);
         })
     });
 });
